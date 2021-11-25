538d3af8e20e8fe994115cfe2ae33e48f50716e8
John <mrjohnskelton@gmail.com>
2021-11-25 20:24:53 +0000

    Recreated basic py having trashed previous version

---
+++ py/connectionTest.py
@@ -0,0 +1,81 @@
+import re
+import subprocess
+import helpers
+import json
+import platform
+import asyncio
+import os
+
+
+# Extract float from string using regular expression
+def extractFloat(string):
+    """
+      For each regex:
+        $1 = The whole number
+        $2 = Integer part
+        $3 = Fractional part with leading period
+        $4 = Fractional part
+    """
+    integerOrFloat = "(?=.)([+-]?([0-9]*)(\\.([0-9]+))?)"
+    reString = ''.join([
+        "Minimum = ", integerOrFloat, "ms, ",
+        "Maximum = ", integerOrFloat, "ms, ",
+        "Average = ", integerOrFloat, "ms"
+    ])
+    # Given pattern above, would return 3x 4 groups
+    # we only want 1, 5 and 9 then
+    return re.search(reString, string).group(1, 5, 9)
+
+
+# Ping a host
+def pingHost(host):
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower() == 'windows' else '-c'
+
+    ping = subprocess.Popen(
+        ["ping", param, "1", host],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    out, error = ping.communicate()
+    # If ping is successful
+    if error == b'':
+        # Convert string to array of strings
+        out = out.decode('utf-8').split('\n')
+        # Iterate of each line in out
+        for line in out:
+            # If line contains "Average"
+            if 'Average' in line:
+                # Return RTT
+                return extractFloat(line)
+    # If ping is unsuccessful
+    else:
+        # Return -1
+        return -1
+
+###############################################################################
+# Main
+###############################################################################
+
+
+config = helpers.getConfig()
+min, max, av = pingHost(config['host'])
+# Get guid
+uuid = helpers.getGuid()
+
+output = {}
+output['uuid'] = uuid
+output['min'] = float(min)
+output['max'] = float(max)
+output['av'] = float(av)
+output['zip'] = config['zip']
+output['countryCode'] = config['countryCode']
+output['timestamp'] = helpers.getTimestamp()
+
+if 'statusPage' in config:
+    loop = asyncio.get_event_loop()
+    coroutine = helpers.getStatus(config['statusPage'])
+    output['status'] = loop.run_until_complete(coroutine)
+
+# Send output to API over http
+if not helpers.sendOutputToAPI(output):
+    helpers.saveToFile(output)
---
+++ py/helpers.py
@@ -0,0 +1,103 @@
+import os
+import json
+import datetime
+import pyppeteer
+import requests
+import uuid
+
+
+def getPwd():
+    # Get current directory in cross-platform safe way
+    return os.path.dirname(os.path.realpath(__file__))
+
+
+# If local config does not exist then open alternative
+def getConfig():
+    pwd_ = getPwd()
+    # get directory seperator in cross-platform safe way
+    sep = os.path.sep
+    # Construct file path to config using join
+    filePath = ''.join([pwd_, sep, 'config.local.json'])
+    # Check if file exists
+    if not os.path.isfile(filePath):
+        filePath = ''.join([pwd_, sep, 'config.json'])
+    # Load config from json file
+    with open(filePath) as json_file:
+        config = json.load(json_file)  # Load config from json file
+    return config
+
+
+# Return ISO 8601 formatted date
+def getTimestamp():
+    return datetime.datetime.now().isoformat()
+
+
+# Use pyppeteer to extract an element from a web page
+async def getStatus(url):
+    # Create a browser
+    browser = await pyppeteer.launch()
+    # Create a new page
+    page = await browser.newPage()
+    # Navigate to url
+    await page.goto(url)
+    # Get the status of the page
+    selector = "#status_tile > ul > .ng-binding"
+    # Extract element using selector
+    element = await page.querySelector(selector)
+    if element:
+        status = await page.evaluate('(element) => element.innerText', element)
+    else:
+        status = 'Unknown'
+    # Close the browser
+    await browser.close()
+    return status
+
+
+# Post output to AWS API over http
+def sendOutputToAPI(output):
+    # Get config
+    config = getConfig()
+    # Get url
+    url = config['endpoint']['url']
+    # Get headers
+    headers = config['endpoint']['headers']
+    # Send data to API
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(output))
+        # Check if response is successful
+        if response.status_code == 200:
+            # If successful then return true
+            return True
+        else:
+            # If unsuccessful then return false
+            return False
+    except Exception as e:
+        print(e)
+        # If exception then return false
+        return False
+
+
+# Get globally unique identifier
+def getGuid():
+    return str(uuid.uuid4())
+
+
+# Save to file as couldn't send to API
+def saveToFile(output):
+    # Get config
+    config = getConfig()
+    # Get directory seperator in cross-platform safe way
+    sep = os.path.sep
+    pwd_ = getPwd()
+    # Construct file path to config using join
+    filePath = ''.join([
+          pwd_, sep,
+          config['failToSendPath']])
+    # Check if dir exists
+    if not os.path.isdir(filePath):
+        # If not then create it
+        os.makedirs(filePath)
+    # Save to file
+    with open(''.join([
+            filePath, sep, output['uuid'], '.json']), 'w') as outfile:
+        json.dump(output, outfile)
