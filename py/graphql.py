c36d2c1387a79fcfe4f654335c015a10a6b61737
John <mrjohnskelton@gmail.com>
2021-11-26 15:35:51 +0000

    Deprecated. Gradually refactoring out functionality

--- py/graphql.py
+++ py/graphql.py
@@ -9,42 +9,7 @@ baseDir= os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())
 with open(os.path.join(baseDir, '..', 'config.json')) as config_file:
     config = json.load(config_file)
 
-# The GraphQL query (with a few aditional bits included) itself defined as a multi-line string.       
-query = """
-{
-  query: listPingTestResults {
-    items {
-      id
-      countryCode
-      postalCode
-      failed
-      min
-      avg
-      max
-      mdev
-      datetime
-    }
-  }
-}
-"""
 
-#See https://stackoverflow.com/questions/48693825/making-a-graphql-mutation-from-my-python-code-getting-error
-#found from: https://www.google.co.uk/search?hl=en&q=python+post+graphql+mutation&meta=
-#
-mutation = """
-mutation create($createpingtestresultsinput: CreatePingTestResultsInput!) {
-  createPingTestResults(input: $createpingtestresultsinput) {
-    id
-    countryCode
-    postalCode
-    failed
-    min
-    avg
-    max
-    mdev
-    datetime
-  }
-}
 
 """
 #See https://stackoverflow.com/questions/48693825/making-a-graphql-mutation-from-my-python-code-getting-error
@@ -67,21 +32,6 @@ def __run_query(url, headers, query):
       return request.json()
   else:
       raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
-
-def createPing(payload):
-  try:
-    #result = run_query(query) # Execute the query
-    variable = {}
-    variable['createpingtestresultsinput'] = payload
-    request = requests.post(config['endpoint']['url'], json={'query': mutation, 'variables': variable}, headers=config['endpoint']['headers'])
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, mutation))
-
-  except requests.exceptions.SSLError:
-    print('No internet connection?')
 
 
 def batchCreatePing(payload):
